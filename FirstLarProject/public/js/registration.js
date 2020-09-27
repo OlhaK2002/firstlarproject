66b1bedb255ce719a49f4732159a9db96dffa986
OlhaK2002 <oliakozlovska02@gmail,com>
2020-09-28 00:28:40 +0300

    36(28.09.2020)

--- FirstLarProject/public/js/registration.js
+++ FirstLarProject/public/js/registration.js
@@ -33,15 +33,16 @@ $(document).ready(function () {
                 console.log(result1);
                 if (result1['success'] === "success") {
                     window.location.href = "/";
-                } else {
-                    $.each(result1, function (parametr, array) {
-                        $.each(array, function (index, value) {
-                            if (value === "The email has already been taken.") $('#error_email').append(result1[index]);
-                            else if (value === "The login has already been taken.") $('#error_login').append(result1[index]);
-                            else if (value === "The password1 must be at least 6 characters.") $('##error_password').append(result1[index]);
-                            else if (value === "The password2 and password1 must match.") $('#error_passwords').append(result1['error_passwords']);
+                }
+                else {
+
+                        $.each(result1.error, function (index, value) {
+                            if (value === "The email has already been taken.") $('#error_email').append(result1.error[index]);
+                            else if (value === "The login has already been taken.") $('#error_login').append(result1.error[index]);
+                            else if (value === "The password1 must be at least 6 characters.") $('#error_password').append(result1.error[index]);
+                            else if (value === "The password2 and password1 must match.") $('#error_passwords').append(result1.error[index]);
                         });
-                    });
+
                     $('#error_password1').append(result1['error_password1']);
                     $('#Password1').val('');
                     $('#Password2').val('');
