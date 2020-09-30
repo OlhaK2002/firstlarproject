10a7dae0c5956f06d3b01c04751438f7ac225f9c
OlhaK2002 <oliakozlovska02@gmail,com>
2020-09-30 10:48:42 +0300

    55(29.09.2020)

--- FirstLarProject/app/Http/Controllers/CommentController.php
+++ FirstLarProject/app/Http/Controllers/CommentController.php
@@ -7,18 +7,24 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new Comment();
+    }
+
     public function comment()
     {
-        $model = new Comment();
-        $array = $model->firstComment();
+        $array = $this->model->firstComment();
         return view('comment', compact('array'));
     }
+
     public function reply()
     {
-        $model = new Comment();
-        $model->reply($_POST['text'], $_POST['parent_id'], Auth::id(), $_POST['nesting']);
+        $this->model->reply($_POST['text'], $_POST['parent_id'], Auth::id(), $_POST['nesting']);
 
-        $array = $model->result();
+        $array = $this->model->result();
         if (!empty($array) && $_POST['text'] != "") {
             return view('reply', compact('array'));
         }
